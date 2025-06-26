8d0dc890469b6c2a8c7f6e9b227180d25ba88e30
peasshoter <8966917+peasshoter@users.noreply.github.com>
2025-06-26 11:04:44 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,6 +1,3 @@
-//Thu Jun 26 2025 02:57:22 GMT+0000 (Coordinated Universal Time)
-//Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
-//Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 var st = "{Og2YZb[zkHDrv~#";
 (function (_0x582bec) {
   var _0x39263e = function (_0x48a080, _0xb7c191) {
